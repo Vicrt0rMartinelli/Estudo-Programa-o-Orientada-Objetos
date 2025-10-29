01e561581ea9d82d103ce4292dcd74fda64614c1
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-28 23:52:25 -0300

    Salvamento 001

---
+++ src/fundamentosDaPOO/TesteParaSalvaMento.java
@@ -0,0 +1,8 @@
+package fundamentosDaPOO;
+
+public class TesteParaSalvaMento {
+    static void main(String[] args) {
+        System.out.println("che che re re che che");
+        System.out.println("Gustavo Lima e você");
+    }
+}
